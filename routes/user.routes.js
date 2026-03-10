164598bc73168efb785cc6bd5a5d6dd5466777b4
itsaivivek <er.vivek.social@gmail.com>
2026-03-10 08:08:15 +0545

    Setup tailwindcss and flowbite in register.ejs

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 
 /* /user/test */
-router.get('/test', (req, res) => {
-  res.send('user Test route')
-}
-)
+// router.get('/test', (req, res) => {
+//   res.send('user Test route')
+// }
+// )
 
+router.get('/register', (req,res)=>{
+    res.render('register');
+})
 
 module.exports = router;
